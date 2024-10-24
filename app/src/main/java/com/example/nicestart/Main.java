b7e6d98cd0ad0336b30eb160d94813a80b8be47f
Marina <marina.sierra3@educa.madrid.org>
2024-10-24 12:09:44 +0200

    Modifying main activity--Profile

--- app/src/main/java/com/example/nicestart/Main.java
+++ app/src/main/java/com/example/nicestart/Main.java
@@ -1,13 +1,16 @@
 package com.example.nicestart;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 public class Main extends AppCompatActivity {
 
     @Override
@@ -15,6 +18,16 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.main_activity);
+
+        ImageView prof = findViewById(R.id.profileImg);
+        Glide.with(this)
+                        .load("https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-profile-picture-female-icon.png")
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+                //.centerCrop()
+                .circleCrop() //CIRCULO
+                //.placeholder(new ColorDrawable(this.getResources().getColor(R.color.CornflowerBlue)))
+                .into(prof);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
