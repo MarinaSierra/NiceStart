54e7fa7023631f0957d1bf6f07f6afeeb04e0e38
Marina <marina.sierra3@educa.madrid.org>
2024-10-24 12:10:30 +0200

    Implemented glide for imageview

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -1,13 +1,19 @@
 package com.example.nicestart;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Login extends AppCompatActivity {
 
@@ -17,7 +23,14 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_activity);
 
-
+        ImageView mGirl=findViewById(R.id.girl);
+        Glide.with(this)
+                        .load("https://media.revistaad.es/photos/61e6fa8210427c62e91838e8/1:1/w_3839,h_3839,c_limit/taisiia-stupak-SFYL9rFcLy0-unsplash.jpg")
+                            .transition(DrawableTransitionOptions.withCrossFade(1000))
+                                .centerCrop()
+                //.circleCrop() CIRCULO
+                                        //.placeholder(new ColorDrawable(this.getResources().getColor(R.color.CornflowerBlue)))
+                                                .into(mGirl);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
--- app/src/main/java/com/example/nicestart/SignUp.java
+++ app/src/main/java/com/example/nicestart/SignUp.java
@@ -1,14 +1,18 @@
 package com.example.nicestart;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class SignUp extends AppCompatActivity {
 
@@ -17,6 +21,15 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.signup_activity);
+
+        ImageView mBoy = findViewById(R.id.boy);
+        Glide.with(this)
+                        .load("https://f.fcdn.app/imgs/3832f5/www.celiafirpo.com.uy/fliluy/2b9d/original/catalogo/FRN012_FRN012_1/1920-1200/ramo-de-flores-naturales-gallerie-ramo-de-flores-naturales-gallerie.jpg")
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.CornflowerBlue)))
+                .into(mBoy);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
