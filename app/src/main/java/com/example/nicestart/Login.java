57236b265666cf297db20c484181416a627cab32
Marina <marina.sierra3@educa.madrid.org>
2024-10-30 12:42:40 +0100

    New activity -- edit profile

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -1,9 +1,10 @@
 package com.example.nicestart;
 
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,13 +17,27 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Login extends AppCompatActivity {
-
+    EditText txt1, txt2;
+    Button bot1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_activity);
+        txt1=findViewById(R.id.username);
+        txt2=findViewById(R.id.password);
 
+        bot1=findViewById(R.id.button1);
+
+        bot1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(Login.this, Profile.class);
+                String value = txt1.getText().toString();
+                intent2.putExtra("Nombre", value);
+                startActivity(intent2);
+            }
+        });
         ImageView mGirl=findViewById(R.id.girl);
         Glide.with(this)
                         .load("https://media.revistaad.es/photos/61e6fa8210427c62e91838e8/1:1/w_3839,h_3839,c_limit/taisiia-stupak-SFYL9rFcLy0-unsplash.jpg")
@@ -42,10 +57,5 @@ public class Login extends AppCompatActivity {
     public void openSignUp(View view) {
         Intent intent1 = new Intent(Login.this, SignUp.class);
         startActivity(intent1);
-    }
-
-    public void openMain(View view) {
-        Intent intent2 = new Intent(Login.this, Main.class);
-        startActivity(intent2);
     }
 }
--- app/src/main/java/com/example/nicestart/SignUp.java
+++ app/src/main/java/com/example/nicestart/SignUp.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,7 +17,8 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class SignUp extends AppCompatActivity {
-
+    Button b;
+    EditText txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +33,18 @@ public class SignUp extends AppCompatActivity {
                 .placeholder(new ColorDrawable(this.getResources().getColor(R.color.CornflowerBlue)))
                 .into(mBoy);
 
+        b=findViewById(R.id.button1);
+        txt=findViewById(R.id.name);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value = txt.getText().toString();
+                Intent intent2 = new Intent(SignUp.this, Profile.class);
+                intent2.putExtra("Nombre", value);
+                startActivity(intent2);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -40,10 +55,5 @@ public class SignUp extends AppCompatActivity {
     public void openLogin(View view) {
         Intent intent1 = new Intent(SignUp.this, Login.class);
         startActivity(intent1);
-    }
-
-    public void openMain(View view) {
-        Intent intent2 = new Intent(SignUp.this, Main.class);
-        startActivity(intent2);
     }
 }
