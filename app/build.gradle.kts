f13265137cdb6f4369942828f65589a925e44653
Marina <marina.sierra3@educa.madrid.org>
2025-01-22 12:56:56 +0100

    bottom_navigation_menu on going

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation("com.airbnb.android:lottie:6.6.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     implementation(libs.swiperefreshlayout)
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     testImplementation(libs.junit)
