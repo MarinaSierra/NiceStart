edf16fd13192a972417d0fc16d47442d0bea12a3
Marina <marina.sierra3@educa.madrid.org>
2025-01-27 12:44:58 +0100

    bottom_navigation_menu.xml finished

--- app/src/main/java/com/example/nicestart/MainBn.java
+++ app/src/main/java/com/example/nicestart/MainBn.java
@@ -1,8 +1,14 @@
 package com.example.nicestart;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,9 +16,12 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.nicestart.ui.main.SectionsPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainBn extends AppCompatActivity {
-
+    private MenuItem prevMenuItem;
     private SectionsPagerAdapter sectionsPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +33,96 @@ public class MainBn extends AppCompatActivity {
 
         ViewPager viewPager1 = findViewById(R.id.view_pager);
         viewPager1.setAdapter(sectionsPagerAdapter);
+        // cast al xml
+        BottomNavigationView mybottomNavView = findViewById(R.id.bottom_navigation);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        // crear badges
+        BottomNavigationMenuView bottomNavigationMenuView =
+                (BottomNavigationMenuView) mybottomNavView.getChildAt(0);
+        View v = bottomNavigationMenuView.getChildAt(2);
+        BottomNavigationItemView itemView = (BottomNavigationItemView) v;
+
+        LayoutInflater.from(this)
+                .inflate(R.layout.layout_badge, itemView, true);
+
+
+        //here we listen to navigation across menu items and set the pager and the icon state checked
+        mybottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId()==R.id.inicio){
+                    item.setChecked(true);
+                    Toast.makeText(MainBn.this, "Inicio", Toast.LENGTH_SHORT).show();
+                    removeBadge(mybottomNavView, item.getItemId());
+                    viewPager1.setCurrentItem(0);
+                } else if (item.getItemId()==R.id.biblioteca) {
+                    item.setChecked(true);
+                    Toast.makeText(MainBn.this, "Biblioteca", Toast.LENGTH_SHORT).show();
+                    removeBadge(mybottomNavView, item.getItemId());
+                    viewPager1.setCurrentItem(1);
+                } else if (item.getItemId()==R.id.anadir) {
+                    item.setChecked(true);
+                    Toast.makeText(MainBn.this, "Añadir", Toast.LENGTH_SHORT).show();
+                    removeBadge(mybottomNavView, item.getItemId());
+                    viewPager1.setCurrentItem(2);
+                } else if (item.getItemId()==R.id.menu) {
+                    item.setChecked(true);
+                    Toast.makeText(MainBn.this, "Menu", Toast.LENGTH_SHORT).show();
+                    removeBadge(mybottomNavView, item.getItemId());
+                    viewPager1.setCurrentItem(3);
+                }
+                return false;
+            }
+        });
+
+
+//        here we listen to viewpager moves and set navigations items checked or not
+
+        viewPager1.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+//                    mybottomNavView.getMenu().getItem(position).setChecked(false);
+                    mybottomNavView.getMenu().getItem(position).setChecked(true);
+                    removeBadge(mybottomNavView, mybottomNavView.getMenu().getItem(position).getItemId());
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (view, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+
+    /**
+     * Remove badge.
+     *
+     * @param bottomNavigationView the bottom navigation view
+     * @param itemId               the item id
+     */
+//removing badges
+    public static void removeBadge(BottomNavigationView bottomNavigationView, @IdRes int itemId) {
+        BottomNavigationItemView itemView = bottomNavigationView.findViewById(itemId);
+        if (itemView.getChildCount() == 3) {
+            itemView.removeViewAt(2);
+        }
+    }
+
 }
+
